72552ae1dc518d8b5eb4cb1b61cfe72a3670edfb
Joe <joeschwehr@gmail.com>
2020-01-06 13:00:07 -0800

    added title to hist

--- plots/d3-and-svg/app.js
+++ plots/d3-and-svg/app.js
@@ -9,10 +9,17 @@ const sv3 = birthData => {
     const barPadding = 2;
     const barWidth = width / numBars;
     const maxBirths = d3.max(birthData, d => d.births);
+    let year = d3.min(birthData, d => d.year);
+
     const yScale = d3
         .scaleLinear()
         .domain([0, maxBirths])
         .range([height, 50]);
+
+    const xScale = d3
+        .scaleLinear()
+        .domain([0, 12])
+        .range([0, width - padding]);
 
     d3.select('#month-input')
         .property('min', minYear)
@@ -36,12 +43,32 @@ const sv3 = birthData => {
         })
         .attr('fill', '#1b69c4');
 
+    sv3.append('text')
+        .attr('class', 'returns-header')
+        .attr('x', width / 2)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '2rem')
+        .text(`Returns for ${year}`);
+
+    let xAxis = sv3
+        .append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0, ${height})`);
+
+    const xAxisCall = d3.axisBottom();
+    xAxis.call(xAxisCall.scale(xScale));
+
     d3.select('#month-input').on('input', () => {
-        const year = +d3.event.target.value;
+        year = +d3.event.target.value;
         sv3.selectAll('rect')
             .data(birthData.filter(d => d.year === year))
+            .transition()
+            .delay((d, i) => i * 50)
             .attr('height', d => height - yScale(d.births))
             .attr('y', d => yScale(d.births));
+
+        sv3.select('.returns-header').text(`Returns for ${year}`);
     });
 };
 
