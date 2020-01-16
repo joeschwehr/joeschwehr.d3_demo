43644da84f0e2e34e24a2e3d15260df1ecc12c70
Joe <joeschwehr@gmail.com>
2020-01-16 12:50:55 -0800

    added data dashboard html and files

---
+++ plots/data-dashboard/js/main.js
@@ -0,0 +1,13 @@
+//Globals
+const DBMARGIN = { top: 0, right: 20, bottom: 40, left: 60 };
+const DBWIDTH = 500 - DBMARGIN.left - DBMARGIN.right;
+const DBHEIGHT = 500 - DBMARGIN.top - DBMARGIN.bottom;
+
+//Events listeners & selections
+
+// console.log(calls);
+console.log('data', db_data);
+
+//handle all of the data loading
+
+//create instances of the vis objects
